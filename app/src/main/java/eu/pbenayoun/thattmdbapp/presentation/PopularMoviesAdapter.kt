33f00c02f7dadc827a671e4ecd950124dfeef8b0
PierreB <pierre@cabnum.fr>
2021-05-08 23:05:09 +0200

    manage rating in UI

--- app/src/main/java/eu/pbenayoun/thattmdbapp/presentation/PopularMoviesAdapter.kt
+++ app/src/main/java/eu/pbenayoun/thattmdbapp/presentation/PopularMoviesAdapter.kt
@@ -2,12 +2,15 @@ package eu.pbenayoun.thattmdbapp.presentation
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.widget.ImageViewCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import eu.pbenayoun.thatdmdbapp.repository.model.TMDBMovie
+import eu.pbenayoun.thattmdbapp.R
 import eu.pbenayoun.thattmdbapp.databinding.ListItemMovieBinding
 
 class PopularMoviesAdapter  :
@@ -23,19 +26,55 @@ class PopularMoviesAdapter  :
         )
     }
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val movie = getItem(position)
-        (holder as PopularMovieViewHolder).binding.title.text= movie.title
-        (holder as PopularMovieViewHolder).binding.releaseDate.text= movie.releaseDate
-        val posterView = (holder as PopularMovieViewHolder).binding.poster
-        Glide.with(posterView.context)
-            .load(movie.posterUrl)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(posterView)
+        (holder as PopularMovieViewHolder).onBind(getItem(position))
     }
 
     class PopularMovieViewHolder(
         val binding: ListItemMovieBinding
-    ) : RecyclerView.ViewHolder(binding.root)
+    ) : RecyclerView.ViewHolder(binding.root){
+
+        fun onBind(tmdbMovie: TMDBMovie){
+
+            binding.title.text= tmdbMovie.title
+            binding.releaseDate.text= tmdbMovie.releaseDate
+            val posterView = binding.poster
+            Glide.with(posterView.context)
+                .load(tmdbMovie.posterUrl)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(posterView)
+            binding.star1.setOnClickListener { setRating(1) }
+            binding.star2.setOnClickListener { setRating(2) }
+            binding.star3.setOnClickListener { setRating(3) }
+            binding.star4.setOnClickListener { setRating(4) }
+            binding.star5.setOnClickListener { setRating(5) }
+        }
+
+        fun setRating(rating : Int){
+            for (index in 1..5){
+                setStarView(index,rating)
+            }
+        }
+
+        private fun setStarView(index:Int, rating:Int) {
+            val isRated = rating>=index
+            when(index){
+                1-> setStar(binding.star1,isRated)
+                2-> setStar(binding.star2,isRated)
+                3-> setStar(binding.star3,isRated)
+                4-> setStar(binding.star4,isRated)
+                5-> setStar(binding.star5,isRated)
+            }
+
+        }
+
+        private fun setStar(imageView: ImageView, isRated: Boolean){
+            val imageResource = when(isRated){
+                false -> R.drawable.ic_star
+                true -> R.drawable.ic_star_rated
+            }
+            imageView.setImageResource(imageResource)
+        }
+    }
 }
 
 private class MovieDiffCallback : DiffUtil.ItemCallback<TMDBMovie>() {
